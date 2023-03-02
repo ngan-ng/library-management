eac8a3ba3f8372041b9c11869f570d1cddae4014
ngan-ng <ngannguyen1399@gmail.com>
2023-03-02 18:56:10 +0700

    add a new book

--- src/main/java/com/application/courselibrary/controller/BookController.java
+++ src/main/java/com/application/courselibrary/controller/BookController.java
@@ -69,4 +69,24 @@ public class BookController {
 
         return "redirect:/books";
     }
+
+    @GetMapping("/add-book")
+    public String addBook(Book book, Model model) {
+        model.addAttribute("categories", categoryService.findAllCategories());
+        model.addAttribute("publishers", publisherService.findAllPublishers());
+        model.addAttribute("authors", authorService.findAllAuthors());
+        return "add-book";
+    }
+
+    @PostMapping("/save-book")
+    public String saveBook(Book book, BindingResult result, Model model) {
+        if(result.hasErrors()){
+            return "add-book";
+        }
+
+        bookService.createBook(book);
+        model.addAttribute("books", bookService.findAllBooks());
+
+        return "redirect:/books";
+    }
 }
