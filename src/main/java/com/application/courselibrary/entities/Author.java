5f834a6325a5fa4aff1009f7b18c59dbb4814cec
ngan-ng <ngannguyen1399@gmail.com>
2023-03-02 11:46:58 +0700

    book details

--- src/main/java/com/application/courselibrary/entities/Author.java
+++ src/main/java/com/application/courselibrary/entities/Author.java
@@ -1,7 +1,6 @@
 package com.application.courselibrary.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
--- src/main/java/com/application/courselibrary/entities/Publisher.java
+++ src/main/java/com/application/courselibrary/entities/Publisher.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
